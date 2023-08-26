d5bfbc089bb3c7f5dbd1a16d8fb8a47132a75206
Dirivial <alexander.kadeby@gmail.com>
2023-08-27 01:52:44 +0200

    Added theme swapping

--- src/pages/my_habits.tsx
+++ src/pages/my_habits.tsx
@@ -1,9 +1,10 @@
 import { useSession } from "next-auth/react";
 import Head from "next/head";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { api } from "~/utils/api";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { type Habit } from "@prisma/client";
+import { themeChange } from "theme-change";
 
 export default function MyHabits() {
   const [newHabit, setNewHabit] = useState("");
@@ -30,6 +31,10 @@ export default function MyHabits() {
   const deleteHabit = api.habit.delete.useMutation({
     onSuccess: () => refetchHabits(),
   });
+
+  useEffect(() => {
+    themeChange(false);
+  }, []);
 
   const isToday = (date: Date) => {
     const today = new Date();
@@ -132,6 +137,7 @@ export default function MyHabits() {
 
   const handleThemeChange = () => {
     console.log("Changed darktheme to ", !darkTheme);
+    //themeChange(!darkTheme);
     setDarkTheme(!darkTheme);
   };
 
@@ -149,44 +155,38 @@ export default function MyHabits() {
           </h1>
 
           <div ref={parent} className="grid w-full grid-cols-1 gap-4 md:gap-8">
-            <div className="flex justify-center gap-x-4">
-              <input
-                type="text"
-                placeholder="New Habit"
-                className="input"
-                value={newHabit}
-                onChange={(e) => setNewHabit(e.target.value)}
-              />
-              <button className="btn btn-primary" onClick={handleCreateHabit}>
-                Add
-              </button>
-
-              <label className="swap-rotate swap">
-                {/* this hidden checkbox controls the state */}
+            <div className="grid w-full grid-cols-3 gap-x-2">
+              <div></div>
+              <div className="flex gap-2">
                 <input
-                  type="checkbox"
-                  value={darkTheme ? "checked" : "unchecked"}
-                  onChange={handleThemeChange}
+                  type="text"
+                  placeholder="New Habit"
+                  className="input flex-grow"
+                  value={newHabit}
+                  onChange={(e) => setNewHabit(e.target.value)}
                 />
-
-                {/* sun icon */}
-                <svg
-                  className="swap-on h-10 w-10 fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-                </svg>
-
-                {/* moon icon */}
-                <svg
-                  className="swap-off h-10 w-10 fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-                </svg>
-              </label>
+                <button className="btn btn-primary" onClick={handleCreateHabit}>
+                  Add
+                </button>
+              </div>
+              <div className="flex justify-end px-2">
+                <div className="dropdown dropdown-end">
+                  <label tabIndex={0} className="btn my-2">
+                    Theme
+                  </label>
+                  <ul
+                    tabIndex={0}
+                    className="menu dropdown-content rounded-box z-[1] my-1.5 w-52 bg-base-200 p-2 shadow"
+                  >
+                    <li>
+                      <a data-set-theme="light">Light</a>
+                    </li>
+                    <li>
+                      <a data-set-theme="dark">Dark</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
             {habits?.map((habit, index) => {
               return (
@@ -229,7 +229,10 @@ export default function MyHabits() {
 
             <div className="modal-action flex justify-between">
               {/* if there is a button in form, it will close the modal */}
-              <button onClick={handleDeleteHabit} className="btn btn-warning">
+              <button
+                onClick={handleDeleteHabit}
+                className="btn btn-warning text-warning-content"
+              >
                 Delete
               </button>
               <button onClick={handleHabitEdited} className="btn">
@@ -294,7 +297,7 @@ function HabitItem(props: HabitItem) {
   }
 
   return (
-    <div className="flex flex-row items-baseline justify-between gap-4 rounded-xl bg-neutral p-4 text-primary-content hover:bg-neutral-focus">
+    <div className="flex flex-row items-baseline justify-between gap-4 rounded-xl bg-base-300 p-4 text-base-content hover:bg-primary-focus hover:text-primary-content">
       <h3 className="text-xl font-bold sm:text-[1.5rem]" onClick={props.edit}>
         {props.name}
       </h3>
